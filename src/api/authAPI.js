a0ce64e0907b26352d0f8095a8d26bae697bc015
VladF7 <v.furnika@gmail.com>
2023-03-13 13:26:12 +0200

    Update authAPI.js

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -1,5 +1,4 @@
 import { request } from "./requestAPI";
-const token = localStorage.getItem('token')
 
 class UserAPI {
     async login (authForm) { 
@@ -14,7 +13,7 @@ class UserAPI {
     }
     async auth () { 
       try {
-        const response = await request(`/api/auth/auth`, 'GET', null, token) 
+        const response = await request(`/api/auth/auth`, 'GET', null, localStorage.getItem('token')) 
         return response
       } catch (error) {
         localStorage.removeItem('token')
